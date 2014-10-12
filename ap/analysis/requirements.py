bb6c4f5e5dcfc5ad2ec4ddf5e8526d072f661621
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-10-12 08:00:39 +0000

    requirement graphs ohmahgawd

---
+++ ap/analysis/requirements.py
@@ -0,0 +1,21 @@
+from ap import db
+from sqlalchemy import func
+import networkx as nx
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+def get_edgelist(s):
+	"""Return edgelist for current Package requirement graph."""
+	return s.query(db.Package.id, db.Requirement.requirement_id).join(db.Release).filter(db.Release.id==db.Requirement.release_id).filter(db.Release.current==True).filter(db.Requirement.requirement.id.__ne__(None)).all()
+
+def degree_rank_chart(edgelist, filename):
+	"""Create a degree rank chart."""
+	g = nx.DiGraph(edgelist)
+	deg_seq = sorted(nx.degree(g).values(), reverse=True)
+	plt.loglog(deg_seq, 'b-')
+	plt.title('Degree rank chart')
+	plt.ylabel('degree')
+	plt.xlabel('rank')
+	plt.savefig(filename)
+	plt.close()
