cfaabf4ef329014438c00f01a1f4fa62b065afec
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-10-22 01:29:49 -0700

    some general+classifier things

--- ap/analysis/classifiers.py
+++ ap/analysis/classifiers.py
@@ -10,3 +10,96 @@ def top_classifiers(s, limit=0):
 	else:
 		# this is a lot!
 		return classifiers
+
+def framework_sizes_by_classifier(s):
+	"""Return dict of Frameworks and their size based on how many packages use their framework classifier."""
+	framework_trove = ['Framework :: BFG',
+		'Framework :: Bob',
+		'Framework :: Bottle',
+		'Framework :: Buildout',
+		'Framework :: Chandler',
+		'Framework :: CherryPy',
+		'Framework :: CubicWeb',
+		'Framework :: Django',
+		'Framework :: Flask',
+		'Framework :: IDLE',
+		'Framework :: IPython',
+		'Framework :: Opps',
+		'Framework :: Paste',
+		'Framework :: Plone',
+		'Framework :: Pylons',
+		'Framework :: Pyramid',
+		'Framework :: Review Board',
+		'Framework :: Scrapy',
+		'Framework :: Setuptools Plugin',
+		'Framework :: Trac',
+		'Framework :: Tryton',
+		'Framework :: TurboGears',
+		'Framework :: Twisted',
+		'Framework :: ZODB',
+		'Framework :: Zope2',
+		'Framework :: Zope3']
+	sizes = {}
+	for f in framework_trove:
+		sizes[f.split(' :: ')[1]] = s.query(db.Classifier.classifier).filter(db.Classifier.classifier==f).count()
+	return sizes
+
+def nonpython_pkgs(s):
+	"""Return a dict of non-python Language classifiers and how many times they are used."""
+	other_classifiers = ['Programming Language :: Ada',
+		'Programming Language :: APL',
+		'Programming Language :: ASP',
+		'Programming Language :: Assembly',
+		'Programming Language :: Awk',
+		'Programming Language :: Basic',
+		'Programming Language :: C',
+		'Programming Language :: C#',
+		'Programming Language :: C++',
+		'Programming Language :: Cold Fusion',
+		'Programming Language :: Cython',
+		'Programming Language :: Delphi/Kylix',
+		'Programming Language :: Dylan',
+		'Programming Language :: Eiffel',
+		'Programming Language :: Emacs-Lisp',
+		'Programming Language :: Erlang',
+		'Programming Language :: Euler',
+		'Programming Language :: Euphoria',
+		'Programming Language :: Forth',
+		'Programming Language :: Fortran',
+		'Programming Language :: Haskell',
+		'Programming Language :: Java',
+		'Programming Language :: JavaScript',
+		'Programming Language :: Lisp',
+		'Programming Language :: Logo',
+		'Programming Language :: ML',
+		'Programming Language :: Modula',
+		'Programming Language :: Objective C',
+		'Programming Language :: Object Pascal',
+		'Programming Language :: OCaml',
+		'Programming Language :: Other',
+		'Programming Language :: Other Scripting Engines',
+		'Programming Language :: Pascal',
+		'Programming Language :: Perl',
+		'Programming Language :: PHP',
+		'Programming Language :: Pike',
+		'Programming Language :: Pliant',
+		'Programming Language :: PL/SQL',
+		'Programming Language :: PROGRESS',
+		'Programming Language :: Prolog',
+		'Programming Language :: REBOL',
+		'Programming Language :: Rexx',
+		'Programming Language :: Ruby',
+		'Programming Language :: Scheme',
+		'Programming Language :: Simula',
+		'Programming Language :: Smalltalk',
+		'Programming Language :: SQL',
+		'Programming Language :: Tcl',
+		'Programming Language :: Unix Shell',
+		'Programming Language :: Visual Basic',
+		'Programming Language :: XBasic',
+		'Programming Language :: YACC',
+		'Programming Language :: Zope']
+	sizes = {}
+	for f in other_classifiers:
+		sizes[f.split(' :: ')[1]] = s.query(db.Classifier.classifier).filter(db.Classifier.classifier==f).count()
+	return sizes
--- ap/analysis/general.py
+++ ap/analysis/general.py
@@ -5,6 +5,7 @@ import networkx as nx
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import re
 
 def pkg_num(s):
 	"""Return total number of packages that actually exist on the index."""
@@ -52,6 +53,60 @@ def top_required_packages(s, top=5):
 	indegs = list(g.in_degree().items())
 	indegs.sort(key=lambda tup: tup[1], reverse=True)
 	named_top = []
-	for i, t in enumerate(indegs[:top]):
+	for t in indegs[:top]:
 		named_top.append([s.query(db.Package).filter(db.Package.id==t[0]).first(), t[1]])
 	return named_top
+
+def find_named_ecosystems(s, cutoff=5):
+	"""Return dict of named ecosystems and their sizes (split by . and - seperators.)"""
+	g = nx.DiGraph(get_pkg_edgelist(s))
+	# Consider something worthy of searching if its indegree is more or equal to cutoff
+	indegs = [i for i in g.in_degree.items() if i[1] => cutoff]
+	search_names = []
+	for t in indegs:
+		pkg_name = s.query(db.Package).filter(db.Package.id==t[0]).first()
+		split_search = re.search('\w+[.-]', pkg_name)
+		if len(split_search.groups()) > 1 and pkg_name.split(split_char)[0] not in search_names:
+			split_char = split_search.group(1)
+			search_names.append(pkg_name.split(split_char)[0])
+	def name_searcher(sep_char):
+		returner = {}
+		for s in search_names:
+			name_count = s.query(db.Package.name).filter(db.Package.name.startswith(s+sep_char)).count()
+			returner[s] = name_count
+		return returner
+	# dot/dash search
+	return {'dot-ecosystems': name_searcher('.'), 'dash-ecosystems': name_searcher('-')}
+
+def framework_sizes_by_classifier(s):
+	"""Return dict of Frameworks and their size based on how many packages use their framework classifier."""
+	framework_trove = ['Framework :: BFG',
+		'Framework :: Bob',
+		'Framework :: Bottle',
+		'Framework :: Buildout',
+		'Framework :: Chandler',
+		'Framework :: CherryPy',
+		'Framework :: CubicWeb',
+		'Framework :: Django',
+		'Framework :: Flask',
+		'Framework :: IDLE',
+		'Framework :: IPython',
+		'Framework :: Opps',
+		'Framework :: Paste',
+		'Framework :: Plone',
+		'Framework :: Pylons',
+		'Framework :: Pyramid',
+		'Framework :: Review Board',
+		'Framework :: Scrapy',
+		'Framework :: Setuptools Plugin',
+		'Framework :: Trac',
+		'Framework :: Tryton',
+		'Framework :: TurboGears',
+		'Framework :: Twisted',
+		'Framework :: ZODB',
+		'Framework :: Zope2',
+		'Framework :: Zope3']
+	sizes = {}
+	for f in framework_trove:
+		sizes[f.split(' :: ')[1]] = s.query(db.Classifier.classifier).filter(db.Classifier.classifier==f).count()
+	return sizes
