77b58a51468e5bbcda02b47966b7572f23f36868
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-10-22 19:04:12 -0700

    test

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import json
+# Maybe split front-end and api routes into different files? (not really sure how to do this)
 app = Flask(__name__)
 
 # Front-end routes
