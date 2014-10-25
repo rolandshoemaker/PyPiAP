6a241f64581a6fe084ece105807bff05732c23a9
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-10-24 17:27:24 -0700

    better resource building

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from ap import db
+from ap import db, config
 
 from flask import Flask, request, render_template, jsonify, url_for
 import urllib.parse
@@ -32,12 +32,13 @@ def options_object(prefix, options):
 # return all the top level resources
 @app.route('/api/v1/')
 def api_index():
-	resources = {'url': config.url+'api/v1/',
+	resources = {'url': config.url+'/api/v1',
 		'resources': {}
 	}
-	endpoints = [url_for(i) for i in app.url_map.iter_rules() if urllib.parse.urlparse(url_for(i)).path.startswith('/api/v1/') and len(urllib.parse.urlparse(url_for(i)).path.split('/')) == 4]
+	endpoints = [config.url+str(i) for i in app.url_map.iter_rules() if str(i).startswith('/api/v1/') and len(str(i).split('/')) == 4]
 	for e in endpoints:
 		if not e.split('/')[-1] == '':
+			resources['resources'][e.split('/')[-1]] = {}
 			resources['resources'][e.split('/')[-1]]['url'] = e
 	return jsonify(resources)
 
@@ -55,8 +56,8 @@ general_options = ['all',
 	'home_page_domains']
 @app.route('/api/v1/general')
 def api_general():
-	resources = {'url': config.url+'api/v1/general',
-		'resources': options_object('api/v1/general/', general_options)
+	resources = {'url': config.url+'/api/v1/general',
+		'resources': options_object('/api/v1/general/', general_options)
 	}
 	return jsonify(resources)
 
@@ -95,4 +96,5 @@ def api_tarballs():
 	pass
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
